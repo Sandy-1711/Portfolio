78d75abd83b2f6bd8f9655906de53bcf2b79c8ab
Sandy-1711 <sandy1711003@gmail.com>
2023-11-16 23:41:39 +0530

    linting error solved

--- src/app/components/Section1/Section1.js
+++ src/app/components/Section1/Section1.js
@@ -31,17 +31,17 @@ export default function Section1() {
                 <span>,</span>
 
             </h2>
-            <p>"I am Sandeep Singh, an experienced frontend developer with a passion for crafting engaging user experiences. Welcome to my portfolio." </p>
+            <p>&quot;I am Sandeep Singh, an experienced frontend developer with a passion for crafting engaging user experiences. Welcome to my portfolio.&quot; </p>
         </div>
         <div className={section1css.portrait}>
             <Image height={1000} width={1000} src="/sandy.jpg" alt='sandy' />
         </div>
         <div className={section1css.text2}>
-            <p>With <strong>2 years</strong> of experience and a strong foundation in web development, I am a dedicated developer. I'm skilled in languages like HTML, CSS and Javascript. I'm enthusiastic about staying informed on the latest trends while eagerly embracing new technologies and frameworks.</p>
+            <p>With <strong>2 years</strong> of experience and a strong foundation in web development, I am a dedicated developer. I&apos;m skilled in languages like HTML, CSS and Javascript. I&apos;m enthusiastic about staying informed on the latest trends while eagerly embracing new technologies and frameworks.</p>
             <p>I have developed numerous visually stunning and satisfying frontend websites, incorporating cutting-edge technologies reminiscent of those featured on <strong>Awwwards.</strong></p>
-            <p>I've adeptly crafted a collection of full-stack projects that operate seamlessly, incorporating authentication, data association, and other relevant features. These projects were developed using languages such as <strong>Node.js, MongoDB, Mongoose, Express, React and Next.</strong></p>
-            <p>I've worked with <strong>startup agencies</strong> and my contribution to them have positively impacted their businesses.</p>
-            <p>I'm deeply passionate about the <strong>Open Source</strong> community and actively engage in contributing to various open-source projects.</p>
+            <p>I&apos;ve adeptly crafted a collection of full-stack projects that operate seamlessly, incorporating authentication, data association, and other relevant features. These projects were developed using languages such as <strong>Node.js, MongoDB, Mongoose, Express, React and Next.</strong></p>
+            <p>I&apos;ve worked with <strong>startup agencies</strong> and my contribution to them have positively impacted their businesses.</p>
+            <p>I&apos;m deeply passionate about the <strong>Open Source</strong> community and actively engage in contributing to various open-source projects.</p>
             <div className={section1css.contactButton}>
                 <a href='/contact'><button>Contact</button></a>
             </div>
